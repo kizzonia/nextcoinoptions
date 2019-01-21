cf7717bd1959c48819c4ed9ac799a7358460f2ca
kizzonia <chibuikefortune@hotmail.com>
2019-01-21 17:22:04 +0100

    btc update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable :confirmable, :lockable
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable
 
   has_many :deposits
   has_many :Withdraws
