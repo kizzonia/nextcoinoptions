7f62b3357ac5dec60f70835574eb0caec4a4912f
kizzonia <chibuikefortune@hotmail.com>
2019-01-23 11:11:54 +0100

    purechat

--- app/mailers/deposit_mailer.rb
+++ app/mailers/deposit_mailer.rb
@@ -1,2 +1,8 @@
 class DepositMailer < ApplicationMailer
+  default :from => "support@expresstradeinvestment.com"
+  def deposit_email(user, deposit)
+    @user = user
+    @deposit = deposit
+     mail(to: "support@nextcoinoptions.com", subject: 'deposit Request')
+  end
 end
--- app/mailers/withdraw_mailer.rb
+++ app/mailers/withdraw_mailer.rb
@@ -1,2 +1,8 @@
 class WithdrawMailer < ApplicationMailer
+  default :from => "support@expresstradeinvestment.com"
+  def withdraw_email(user, withdraw)
+    @user = user
+    @withdraw = withdraw
+     mail(to: "support@nextcoinoptions.com", subject: 'withdraw Request')
+  end
 end
