b52a90ff6956fb4259aaff79e9ba12017fde4271
kizzonia <chibuikefortune@hotmail.com>
2019-01-25 21:36:54 +0100

    finally

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,11 +1,12 @@
 if Rails.env.production?
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'nextcoinoptions.herokuapp.com',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
   :authentication => :plain,
+  :enable_starttls_auto => true
 }
 end
